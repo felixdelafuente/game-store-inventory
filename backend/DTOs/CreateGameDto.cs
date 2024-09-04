7cb599f4aa8dedf48956ed8b184133d6a87a1159
Felix Dela Fuente <felixjerome.delafuente@gmail.com>
2024-09-04 21:22:46 +0800

    feat(be): add new game

---
+++ backend/DTOs/CreateGameDto.cs
@@ -0,0 +1,8 @@
+namespace backend.DTOs;
+
+public record class CreateGameDto(
+  string Name,
+  string Genre,
+  decimal Price,
+  DateOnly ReleaseDate
+);
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,8 +1,11 @@
 using backend.DTOs;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+const string GetGameEndpointName = "GetGame";
 
 List<GameDto> games = [
   new (
@@ -28,8 +31,28 @@ List<GameDto> games = [
   ),
 ];
 
+// GET /games
 app.MapGet("/games", () => games);
 
-app.MapGet("/", () => "Hello World!");
+// GET /games/1
+app.MapGet("/games/{id}", (int id) => games.Find(game => game.Id == id))
+  .WithName(GetGameEndpointName);
+
+
+// POST /games
+app.MapPost("/games", (CreateGameDto newGame) =>
+{
+  GameDto game = new(
+    games.Count + 1,
+    newGame.Name,
+    newGame.Genre,
+    newGame.Price,
+    newGame.ReleaseDate
+  );
+
+  games.Add(game);
+
+  return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+});
 
 app.Run();
